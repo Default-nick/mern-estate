e25ba5d39cb4a47afce4e7aacdb5949f6fef939f
Leandro Alves <lalvesevangelista@gmail.com>
2023-10-06 16:42:50 -0300

    Feat: Conexão com banco de dados MongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connect to MongoDB!"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
