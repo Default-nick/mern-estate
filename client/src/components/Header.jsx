0b9d2a977002239c7b9c133a3c9fc51b38362d31
Leandro Alves <lalvesevangelista@gmail.com>
2023-10-06 16:29:05 -0300

    Refactor: Mudança no nome da aplicação

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
             <Link to='/'>
             <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
                 <span className="text-slate-500">
-                    Sahand
+                    Mern
                 </span>
                 <span className="text-slate-700">
                     Estate
