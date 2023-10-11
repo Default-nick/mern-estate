8e45544d9afeedebec78fa91422d604830c66b80
Leandro Alves <lalvesevangelista@gmail.com>
2023-10-11 14:36:10 -0300

    Feat: Criação de formulário de página de Sign In

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,25 @@
+import {Link} from "react-router-dom";
+
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">
+        SignUp
+      </h1>
+      <form className="flex flex-col gap-4">
+        <input type="text" placeholder="username" className="border p-3 rounded-lg" id='username'/>
+        <input type="email" placeholder="email" className="border p-3 rounded-lg" id='email'/>
+        <input type="password" placeholder="password" className="border p-3 rounded-lg" id='password'/>
+        <button  
+        className="bg-slate-700 text-white p-3 rounded-lg 
+        uppercase hover:opacity-90 disabled:opacity-50 disabled:cursor-not-allowed"
+        >Sign Up</button>
+        <div className="flex gap-2 mt-2">
+          <p>Have an account?</p>
+          <Link to="/sign-in">
+            <span className="text-blue-700">Sign in</span></Link>
+        </div>
+      </form>
+    </div>
   )
 }
