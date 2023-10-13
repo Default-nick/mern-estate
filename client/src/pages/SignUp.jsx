d443ac3b251a7a0670b468d516aaabec86bac324
Leandro Alves <lalvesevangelista@gmail.com>
2023-10-13 19:17:12 -0300

    Feat: Criação de página de login funcional Refactor: Correção de escrita em página de cadastro

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -44,7 +44,7 @@ export default function SignUp() {
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">
-        SignUp
+        Sign Up
       </h1>
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input type="text" placeholder="username" className="border p-3 rounded-lg" id='username' onChange={handleChange}/>
@@ -58,7 +58,7 @@ export default function SignUp() {
         <div className="flex gap-2 mt-2">
           <p>Have an account?</p>
           <Link to="/sign-in">
-            <span className="text-blue-700">Sign in</span></Link>
+            <span className="text-blue-700">Sign Up</span></Link>
         </div>
       </form>
       {error && <p className="text-red-500 mt-5">{error}</p>}
