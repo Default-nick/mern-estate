86746d3e44b55afe0daf643ae2f55a7006095748
Leandro Alves <lalvesevangelista@gmail.com>
2023-10-17 15:54:33 -0300

    Feat: Adicionado foto de perfil no header. Feat: Adicionado regras de navegação para rota privada.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 export default function Header() {
+    const {currentUser} = useSelector(state => state.user)
+    console.log(currentUser)
+
   return (
     <header className="bg-slate-200 shadow-md">
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -21,13 +25,19 @@ export default function Header() {
             </form>
             <ul className='flex gap-4'>
                 <Link to="/">
-                <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+                    <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
                 </Link>
                 <Link to="/about">
-                <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+                    <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                 </Link>
-                <Link to="/sign-in">
-                <li className='hidden sm:inline text-slate-700 hover:underline'>Sign in</li>
+                <Link to={currentUser ? "/profile" :"/sign-in"}>
+                {currentUser ? (
+                    <img 
+                        src={currentUser.avatar} alt={`${currentUser.username}'s photo`} 
+                        className='rounded-full h-7 w-7 object-cover'/>
+                ) : (
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>Sign in</li>
+                    )}
                 </Link>
             </ul>
         </div>
