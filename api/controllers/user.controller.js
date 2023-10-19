e4fb6ef7947803f539fe767682dce8a3661afa7e
Leandro Alves <lalvesevangelista@gmail.com>
2023-10-19 17:12:37 -0300

    Feat: Criação de rota de edição de usuário na API.

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,43 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
   res.json({
     message: "Hello mai besto friendo",
   });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(
+      errorHandler(
+        401,
+        "I knew you would try this... You can only update your own account, pal."
+      )
+    );
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updateUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
