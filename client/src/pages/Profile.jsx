c51e4c13b1d2e697e50939dab05143ace033bb89
Leandro Alves <lalvesevangelista@gmail.com>
2023-10-25 22:05:50 -0300

    Feat: Adicionada de página de criação anúncio.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from 'react'
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from '../firebase'
 import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from '../redux/user/userSlice';
+import {Link} from 'react-router-dom'
 
 export default function Profile() {
   const {currentUser, loading, error} = useSelector((state)=> state.user);
@@ -161,6 +162,10 @@ export default function Profile() {
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? 'Loading...' : 'Update'}
         </button>
+
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to="/create-listing">
+          Create Listing
+        </Link>
       </form>
 
       <div className='flex justify-between my-5'>
