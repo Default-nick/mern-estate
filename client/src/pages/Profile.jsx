87e9a79670a204c546cbae3c121e2d6268f7acef
Leandro Alves <lalvesevangelista@gmail.com>
2023-11-01 16:53:43 -0300

    Feat: Adicionado campo de listagem de anúncios.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,8 @@ export default function Profile() {
   const [fileUploadError, setUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false)
+  const [showListingsError, setShowListingsError] = useState(false)
+  const [userListings, setUserListings] = useState([])
 
   // firebase storage
   // allow read;
@@ -117,6 +119,21 @@ export default function Profile() {
     }
   }
 
+  const handleShowListings = async()=>{
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`)
+      const data = await res.json();
+      if(data.success === false){
+        setShowListingsError(true);
+        return
+      }
+      setUserListings(data)
+    } catch (error) {
+      setShowListingsError(true)
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -177,10 +194,35 @@ export default function Profile() {
           Sign Out
         </span>
       </div>
-      <div className='flex justify-center'>
-      <p className='text-red-700 mt-5 font-semibold'>{error ? error : ""}</p>
-      <p className='text-green-700 mt-5 font-semibold'>{updateSuccess ? 'User is updated successfully!' : ""}</p>
-      </div>
+
+        <p className='text-red-700 mt-5 font-semibold text-center'>{error ? error : ""}</p>
+        <p className='text-green-700 mt-5 font-semibold text-center'>{updateSuccess ? 'User is updated successfully!' : ""}</p>
+
+      <button onClick={handleShowListings} className='text-green-700 w-full font-semibold'>Show Listings</button>
+      <p className='text-red-700 mt-5 font-semibold text-center'>{showListingsError ? 'Error showing listings' : ''}</p>
+      {userListings && userListings.length > 0 && 
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center text-2xl font-semibold'>Your Listings</h1>
+          {userListings.map((listing)=>(
+            <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+              <Link to={`/listing/${listing._id}`}>
+                <img src={listing.imageUrls[0]} alt='Listing image' className='h-16 w-16 object-contain'/>
+              </Link>
+              <Link to={`/listing/${listing._id}`} className='text-slate-700 font-semibold flex-1 hover:underline truncate'>
+                <p>{listing.name}</p>
+              </Link>
+              <div className='flex flex-col items-center'>
+                <button className='text-red-700 uppercase'>
+                  Delete
+                </button>
+                <button className='text-green-700 uppercase'>
+                  Edit
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      }
     </div>
   )
 }
