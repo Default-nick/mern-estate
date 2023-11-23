11059edbbf8819e5363e50cec246d5d1d1271e2b
Leandro Alves <lalvesevangelista@gmail.com>
2023-11-23 18:34:28 -0300

    Feat: Adicionado pré instalação para deploy

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("Connect to MongoDB!"))
   .catch((err) => console.log(err));
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -26,6 +29,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.statis(path.join(__dirnamem, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
